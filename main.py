790a8307d4fd083d04f2a368991f0a9026b46eac
Bárbara Flores <bpf17@duke.edu>
2026-05-26 20:26:45 -0700

    Track new jobs incrementally

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from src.pipeline import run_pipeline
 
 
 if __name__ == "__main__":
-    run_pipeline()
+    new_jobs = run_pipeline()
+
+    if len(new_jobs) > 0:
+        print("\nNew jobs found:")
+        print(new_jobs[["title", "location", "url"]])
+    else:
+        print("\nNo new jobs found")
